6c63fed860e415ed8071d6af8b9ae73528459ba4
Daniel Ramos de Oliveira Tenório Bomfim <58080460+debora633@users.noreply.github.com>
2026-09-12 18:31:49 -0300

    sintaxe assembly tipo R e tipo J

--- main.py
+++ main.py
@@ -19,10 +19,66 @@ for instrucao in instrucoes:
         shamt = int(binario[21:26], 2)
         funct = int(binario[26:32], 2)
 
+        if funct == 32:
+            assembly = f"add ${rd}, ${rs}, ${rt}"
+        elif funct == 33:
+            assembly = f"addu ${rd}, ${rs}, ${rt}"
+        elif funct == 36:
+            assembly = f"and ${rd}, ${rs}, ${rt}"
+        elif funct == 26:
+            assembly = f"div ${rs}, ${rt}"
+        elif funct == 27:
+            assembly = f"divu ${rs}, ${rt}"
+        elif funct == 8:
+            assembly = f"jr ${rs}"
+        elif funct == 16:
+            assembly = f"mfhi ${rd}"
+        elif funct == 18:
+            assembly = f"mflo ${rd}"
+        elif funct == 24:
+            assembly = f"mult ${rs}, ${rt}"
+        elif funct == 25:
+            assembly = f"multu ${rs}, ${rt}"
+        elif funct == 39:
+            assembly = f"nor ${rd}, ${rs}, ${rt}"
+        elif funct == 37:
+            assembly = f"or ${rd}, ${rs}, ${rt}"
+        elif funct == 0:
+            assembly = f"sll ${rd}, ${rt}, {shamt}"
+        elif funct == 4:
+            assembly = f"sllv ${rd}, ${rt}, ${rs}"
+        elif funct == 42:
+            assembly = f"slt ${rd}, ${rs}, ${rt}"
+        elif funct == 3:
+            ssembly = f"sra ${rd}, ${rt}, {shamt}"
+        elif funct == 7:
+            assembly = f"srav ${rd}, ${rt}, ${rs}"
+        elif funct == 2:
+            assembly = f"srl ${rd}, ${rt}, {shamt}"
+        elif funct == 6:
+            assembly = f"srlv ${rd}, ${rt}, ${rs}"
+        elif funct == 34:
+            assembly = f"sub ${rd}, ${rs}, ${rt}"
+        elif funct == 35:
+            assembly = f"subu ${rd}, ${rs}, ${rt}"
+        elif funct == 38:
+            assembly = f"xor ${rd}, ${rs}, ${rt}"
+        elif funct == 12:
+            assembly = "syscall"
+        
     #TIPO J
-    elif opcode in (2, 3):
-        target = int(binario[6:32], 2)  
+    elif opcode == 2:
 
+        target = int(binario[6:32], 2)
+
+        assembly = f"j {target}"
+
+    elif opcode == 3:
+
+        target = int(binario[6:32], 2)
+
+        texto = f"jal {target}"
+        
     #TIPO I
     else:
         rs = int(binario[6:11], 2)
